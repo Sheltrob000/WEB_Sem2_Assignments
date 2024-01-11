074970c4e4db18130cdfe17ce551ed2ca78709de
Robert Shelton <sheltrob000@warren.k12.in.us>
2024-01-11 09:13:08 -0500

    document

---
+++ Unit 01_JavaScript_Basics and DOM Manipulation/03_Statements, Syntax, Comments, Variables, and Prompt/script.js
@@ -0,0 +1,5 @@
+let first = prompt("enter your first name");
+let second = prompt("enter your last name");
+
+document.getElementById("name").innerHTML = first + " " + second;
+// document.getElementById("second").innerHTML = second
