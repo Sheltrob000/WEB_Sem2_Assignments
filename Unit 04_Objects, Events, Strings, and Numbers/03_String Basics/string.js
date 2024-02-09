b423b1d8b7d6d25411342bf4f3b75cb09fb6c0f1
Robert Shelton <sheltrob000@warren.k12.in.us>
2024-02-09 08:26:12 -0500

    lyr

---
+++ Unit 04_Objects, Events, Strings, and Numbers/03_String Basics/string.js
@@ -0,0 +1,59 @@
+let ly = `- Crowd: A witch! A witch! A witch! We found a witch! We've got a witch! A witch! A witch! We have found a witch. May we burn her?
+- How do you know she is a witch
+- She looks like one.
+- Bring her forward.
+- I'm not a witch! I'm not a witch !
+- But you are dressed as one.
+- They dressed me like this. - No, we didn't.
+- And this isn't my nose. It's a false one.
+- Well? - We did do the nose.
+- The nose? - And the hat. But she is a witch !
+- Did you dress her up like this? - No, no!
+- Yes. A bit.
+- She has got a wart.
+- What makes you think she's a witch?
+- She turned me into a newt!
+- A newt?
+- I got better.
+- Burn her anyway!
+- Quiet! Quiet!
+- There are ways of telling whether she is a witch.
+- Are there? What are they? Tell us. - Do they hurt?
+- Tell me, what do you do with witches?
+- Burn them!
+- And what do you burn, apart from witches?
+- More witches! - Wood!
+- So why do witches burn?
+- 'Cause they're made of wood? - Good!
+- How do we tell if she is made of wood? - Build a bridge out of her.
+- But can you not also make bridges out of stone?
+- Oh, yeah.
+- Does wood sink in water?
+- No, it floats. - Throw her into the pond!
+- What also floats in water?
+- Bread. - Apples.
+- Very small rocks. - Cider! Great gravy.
+- Cherries. Mud. - Churches.
+- Lead. - A duck!
+- Exactly.
+- So, logically--
+- If she weighs the same as a duck...
+- she's made of wood.
+- And therefore?
+- A witch!
+- A duck! A duck! - Here's a duck.
+- We shaIl use my largest scales.
+- Burn the witch !
+- Remove the supports!
+- A witch!
+- It's a fair cop.
+- Who are you, who are so wise in the ways of science?
+- I am Arthur, king of the Britons.`;
+
+
+
+
+
+
+document.getElementById("lyr").innerHTML = ly;
+// document.write(number)
