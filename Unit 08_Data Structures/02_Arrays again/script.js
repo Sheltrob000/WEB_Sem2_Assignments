b8904c8e6f3ded3fe17836c5ce937320d457863b
Robert Shelton <sheltrob000@warren.k12.in.us>
2024-04-11 09:06:04 -0400

    the list sorted finally

--- Unit 08_Data Structures/02_Arrays again/script.js
+++ Unit 08_Data Structures/02_Arrays again/script.js
@@ -14,17 +14,20 @@
 
 const numbers = []
 
-
 for (let i = 0; i < 10; i++) {
     let num = Math.floor(Math.random() * 10) + 1;
     numbers.push(num);
 }
+let test = 34434323423;
+
 
 function filterOutEvens(number) {
     return number % 2 === 1
 }
 
-let filteredNumbers = numbers.filter(filterOutEvens);
-
+const filteredNumbers = numbers.filter(filterOutEvens);
 
 document.getElementById("unsorted").innerHTML = filteredNumbers;
+const sortedFilteredNumbers = filteredNumbers.sort();
+
+document.getElementById("sorted").innerHTML = sortedFilteredNumbers;
